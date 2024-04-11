80f367c8d1f2a05f3fda9ab49116f01321bea361
Rahul Varshney <36094038+rahulvarshney189@users.noreply.github.com>
2024-04-11 10:26:20 +0530

    Update streamlit_app.py
    added fruit_vice response in a dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 # Import python packages
 import streamlit as st
+import requests
 from snowflake.snowpark.functions import col
+
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -24,6 +26,10 @@ ingredients_list = st.multiselect(
     my_dataframe,
     max_selections = 5
 )
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fv_df = st.dataframe(data = fruityvice_response.json(), use_container_width=True)
+
 if ingredients_list:
     
     ingredients_str = ''
